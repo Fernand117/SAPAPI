24ac95c0bf2590c7109111c7e824fa604bda7828
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2025-06-15 01:01:47 -0600

    feat(usuarios): se añade opción para hacer cambio de contraseña y asignación de contraseña en automático

--- SistemaAutoPartesAPI/Controllers/PermisosSalidaController.cs
+++ SistemaAutoPartesAPI/Controllers/PermisosSalidaController.cs
@@ -2,9 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SistemaAutoPartesAPI.Models;
 using SistemaAutoPartesAPI.Models.DTOs;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SistemaAutoPartesAPI.Controllers
 {
