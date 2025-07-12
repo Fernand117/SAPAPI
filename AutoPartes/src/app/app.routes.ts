7c51dbb01f51da7d6b80c436d5c4e8fbc31ad61f
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2025-07-11 20:26:15 -0600

    feat(autopartes-front): proyecto inicial

--- AutoPartes/src/app/app.routes.ts
+++ AutoPartes/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login', component: LoginComponent
+    },
+    {
+        path: 'inicio', component: InicioComponent
+    },
+    {
+        path: '**', pathMatch: 'full', redirectTo: 'inicio'
+    }
+];
